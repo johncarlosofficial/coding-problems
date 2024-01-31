3bc779fa6a4956682a979bc6a28af66cf325f0fb
johncarlosofficial <johncarlosdeveloper@gmail.com>
2024-01-31 14:10:31 +0000

    docs(check_permutation): add explanatory comments to improve code readability

--- arrays_and_strings/check_permutation.py
+++ arrays_and_strings/check_permutation.py
@@ -4,16 +4,21 @@
 class Solution:
 
   def check_permutation(self, s, t) -> bool:
+    # Initialize frequency maps for characters in both strings
     s_map = {}
     t_map = {}
 
+    # Populate frequency map for string s
     for char in s:
       s_map[char] = s_map.get(char, 0) + 1
 
+    # Populate frequency map for string t
     for char in t:
       t_map[char] = t_map.get(char, 0) + 1
 
+    # Check if the frequency maps are equal, indicating a permutation
     if s_map == t_map:
       return True
 
+    # If frequency maps are not equal, strings are not permutations
     return False
